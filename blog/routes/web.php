85d262228752736921f191513d28ad12efd9ad5a
rupomsoft <33941621+rupomsoft@users.noreply.github.com>
2019-11-29 16:26:47 +0600

    Part 06 Route Prameter
    Laravel Rest API Bangla Tutorial | Part 06 Route Prameter

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -1,27 +1,7 @@
 <?php
 
-$router->get('/get', function () use ($router) {
-    return "I am get";
-});
+$router->post('/{name}/{age}[/{city}]',function($name,$age,$city=null){
 
-
-$router->post('/post', function () use ($router) {
-    return "I am post";
-});
-
-
-$router->put('/put', function () use ($router) {
-    return "I am put";
-});
-
-
-$router->delete('/delete', function () use ($router) {
-    return "I am delete";
-});
-
-
-$router->post('/rabbil',function(){
-
-   return "I am Rabbil Hasan";
+return $name.$age.$city;
 
 });
