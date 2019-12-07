0ef029314169177494ff7997fb56dec7e74e63c5
rupomsoft <33941621+rupomsoft@users.noreply.github.com>
2019-12-07 20:45:35 +0600

    Part 20 Check DB Connection
    Part 20 Check DB Connection

--- blog/app/Http/Controllers/ExampleController.php
+++ blog/app/Http/Controllers/ExampleController.php
@@ -1,18 +1,14 @@
 <?php
 
 namespace App\Http\Controllers;
+use illuminate\support\Facades\DB;
 
 class ExampleController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        //
-    }
+    
 
-    //
+    function testConn(){
+            $dbname=DB::Connection()->select("SELECT * FROM details");
+      return $dbname;
+    }
 }
